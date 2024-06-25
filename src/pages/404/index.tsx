51acdccfdf9b4d5719778849c7e8f15da3c8c521
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2024-06-25 17:47:00 -0300

    feat: 404 page

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+
+export const NotFoundPage = () => {
+  return (
+    <main
+      className={cn("container flex min-h-dvh items-center justify-center p-4")}
+    >
+      <div className="max-h-[49]px">
+        <h1
+          className={cn(
+            "mr-[20px] inline-block border-r border-solid border-black border-opacity-30 pr-[23px] align-top text-2xl font-semibold leading-[49px]"
+          )}
+        >
+          404
+        </h1>
+        <h2
+          className={cn("m-0 inline-block text-base font-light leading-[49px]")}
+        >
+          Esta página não pôde ser encontrada.
+        </h2>
+      </div>
+    </main>
+  );
+};
