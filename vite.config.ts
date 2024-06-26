6420353054f0c0d53532829373d94511a7c9717f
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2024-06-26 20:44:47 -0300

    ci: improved bundle spliting

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,31 @@
 import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+import { dependencies } from "./package.json";
+
+function renderChunks(deps: Record<string, string>) {
+  const chunks: Record<string, string[]> = {};
+  Object.keys(deps).forEach((key) => {
+    if (["react", "react-router-dom", "react-dom"].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-router-dom", "react-dom"],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
